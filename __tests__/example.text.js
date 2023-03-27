69d2856bc4460eef517f36f20054151067cf24d2
Oscar Sandoval <oscarjs707@gmail.com>
2023-03-27 15:05:23 -0700

    Added test files

--- __tests__/example.text.js
+++ __tests__/example.text.js
@@ -1,4 +1,25 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1+2).toBe(3);
+const app = require('../server/app');
+const request = require("supertest");
+
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(1+2).toBe(3);
+// });
+
+
+describe('test getting server responses', ()=>{
+  test('get answers', ()=>{
+    return request(app)
+      .get('/qa/questions/1/answers')
+      .then(response =>{
+        expect(response.statusCode).toBe(200);
+      });
+  });
+  test('get questions', ()=>{
+    return request(app)
+      .get('/qa/questions/1/1/1')
+      .then(response =>{
+        expect(response.statusCode).toBe(200);
+      });
+  });
 });
 
