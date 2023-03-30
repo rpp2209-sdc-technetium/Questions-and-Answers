c8b805cbf71fcef04d76e3fc8f5a4c71e12cab76
Oscar Sandoval <oscarjs707@gmail.com>
2023-03-30 16:29:48 -0700

    Improved get Answers look up

--- db/index.js
+++ db/index.js
@@ -33,9 +33,9 @@ db.getAnswers = (questionID)=>{
   });
 };
 
-db.getPhotos = (answerID)=>{
+db.getPhotos = (condition)=>{
   return new Promise ((fulfill, reject)=>{
-    connection.query(`SELECT * FROM photos WHERE answerID = "${answerID}"`, (err, results, fields)=>{
+    connection.query(`SELECT * FROM photos WHERE ${condition}`, (err, results, fields)=>{
       if (err) {
         reject(err);
       } else {
@@ -118,16 +118,13 @@ helpers.addQuestion = (qInfo)=>{
 
 helpers.getAnswers = (questionID)=>{
 
-  function AnswerObj (answer, photos){
+  function AnswerObj (answer){
     this.answer_id = answer.id;
     this.body = answer.body;
     this.date = answer.date;
     this.answerer_name = answer.answerer_name;
     this.helpfuless = answer.helpfulness;
     this.photos = [];
-    for (var x = 0; x < photos.length; x++) {
-      this.photos.push({id: photos[x].id, url: photos[x].url});
-    }
   };
 
   return new Promise ((fulfill, reject)=>{
@@ -140,22 +137,48 @@ helpers.getAnswers = (questionID)=>{
     //get the answers for the desired question
     db.getAnswers(questionID)
     .then((answers)=>{
-      //loop through the returned answers
 
-      var loop = (x, callback)=>{
-        if (x < answers.length) {
-          db.getPhotos(answers[x].id)
-          .then((photos)=>{
-            data.results.push(new AnswerObj(answers[x], photos));
-            loop(x + 1, callback);
-          });
-        } else {
-          callback();
+      if (answers.length > 0) {
+        //get the photos for each answer
+        //generate the query
+        var condition = ``;
+
+        //loop through the answers
+        for (var x = 0; x < answers.length; x++) {
+          data.results.push(new AnswerObj(answers[x]));
+          condition = condition.concat(`answerID = ${answers[x].id} || `);
         }
-      };
 
-      loop(0, ()=>{fulfill(data);});
 
+        //remove the last comma
+        condition = condition.slice(0, condition.length - 3);
+
+        //get all the photos for all the answers
+        db.getPhotos(condition)
+        .then((photos)=>{
+          //add the photos to their respective answer
+          photosObj = {};
+          for (var x = 0; x < photos.length; x++) {
+            if (photosObj[photos[x].answerID] === undefined) {
+              photosObj[photos[x].answerID] = [{id: photos[x].id, url: photos[x].url}];
+            } else {
+              photosObj[photos[x].answerID].push({id: photos[x].id, url: photos[x].url});
+            }
+          }
+
+          //loop through the answers
+          for (var x= 0; x < data.results.length; x++) {
+            if (photosObj[data.results[x].answer_id] !== undefined) {
+              data.results[x].photos = photosObj[data.results[x].answer_id];
+              console.log(data.results[x].photos);
+            }
+          }
+
+          fulfill(data);
+        });
+      } else {
+        fulfill(data);
+      }
 
     });
   });
